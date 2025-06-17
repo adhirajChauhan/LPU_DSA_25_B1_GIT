b103956b1ab49cdac384658ea89f5924798cd6a0
adhirajChauhan <adhirajsingh0105@gmail.com>
2025-06-17 09:20:03 +0530

    Adding new files

---
+++ CPP/02_Conditional_Statements/01.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    // int num = 5;
+
+    // if(num > 3){
+    //     cout << "1" << endl;
+    // }
+
+    // else{
+    //     cout << "2" << endl;
+    // }
+
+    int a = 5, b = 5;
+
+    if((a++ == 5) && (++b == 6) || (a == 7)){
+        cout << "1" << endl;
+    }
+    else{ cout << "2" << endl;}
+
+    cout << "a = " << a << " b = " << b << endl;
+}
